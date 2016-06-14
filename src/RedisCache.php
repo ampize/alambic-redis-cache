3d93d3e0abceb180edaa34b1a4487716b3b9eaff
Didier Fanchon <didier.fanchon@webtales.fr>
2016-06-14 10:55:37 +0200

    read/write from cache

--- src/RedisCache.php
+++ src/RedisCache.php
@@ -8,14 +8,33 @@ class RedisCache
 {
     public function __invoke($payload=[])
     {
-        $configs=isset($payload["configs"]) ? $payload["configs"] : [];
+        $client = new \Predis\Client();
+        if (!isset($payload["response"])) {
+          $key = $this->array_md5($payload).":response";
+          $value = $client->get($key);
+          if (isset($value)) {
+            // read from cache
+            $payload["response"] = json_decode($value,true);
+          } else {
+            // set cache key for further stages
+            $payload["cacheKey"] = $key;
+          }
+        } else {
+          if (isset($payload["cacheKey"])) {
+            // write to cache
+            $key = $payload["cacheKey"];
+            $value = $payload["response"];
+            $client->set($key,json_encode($value));
+          }
+        }
 
-        client = new Predis\Client();
-        $client->set('foo', 'bar');
-        $value = $client->get('foo');
+      return $payload;
 
-        return $value;
+    }
 
+    private function array_md5(Array $array) {
+        array_multisort($array);
+        return md5(json_encode($array));
     }
 
 }
