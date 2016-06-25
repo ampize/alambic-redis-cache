bacd77a4b18f416d5d844c35a0f5d705992636af
Didier Fanchon <didier.fanchon@webtales.fr>
2016-06-25 15:05:54 +0200

    Add TTL

--- src/RedisCache.php
+++ src/RedisCache.php
@@ -2,76 +2,87 @@
 
 namespace RedisCache;
 
-use \Exception;
-
 class RedisCache
-
 {
-
     protected $_client;
+    protected $_defaultCacheTTL = 3600;
 
-    public function __invoke($payload=[])
+    public function __invoke($payload = [])
     {
+        // Init Redis client
         $this->_client = new \Predis\Client();
-        return $payload["isMutation"] ? $this->execute($payload) : $this->resolve($payload);
+        // Set default cache TTL
+        if (isset($payload['connectorBaseConfig']['defaultCacheTTL'])) {
+            $this->_defaultCacheTTL = $payload['connectorBaseConfig']['defaultCacheTTL'];
+        }
+
+        return $payload['isMutation'] ? $this->execute($payload) : $this->resolve($payload);
     }
 
-    private function resolve($payload=[]) {
-      if (!isset($payload["response"])) {
-        $cacheKey = $payload["pipelineParams"]["currentRequestString"].":response";
-        $value = $this->_client->get($cacheKey);
-        $type = explode("/",$cacheKey)[0];
-        if (isset($value)) {
-          // read from cache
-          $payload["response"] = json_decode($value,true);
+    private function resolve($payload = [])
+    {
+        if (!isset($payload['response'])) {
+            $cacheKey = $payload['pipelineParams']['currentRequestString'].':response';
+            $value = $this->_client->get($cacheKey);
+            $type = explode('/', $cacheKey)[0];
+            if (isset($value)) {
+                // read from cache
+                $payload['response'] = json_decode($value, true);
+            } else {
+                // set cache key for further stages
+                $payload['cacheKey'] = $cacheKey;
+            }
+            // set cache for parent query
+            if (isset($payload['pipelineParams']['parentRequestString'])) {
+                $parentCacheKey = $payload['pipelineParams']['currentRequestString'].':parentQueries';
+                $this->_client->sAdd($parentCacheKey, $payload['pipelineParams']['parentRequestString']);
+            }
+            // set cache for object requests
+            $typeCacheKey = $type.':queries';
+            $this->_client->sAdd($typeCacheKey, $payload['pipelineParams']['currentRequestString']);
         } else {
-          // set cache key for further stages
-          $payload["cacheKey"] = $cacheKey;
+            if (isset($payload['cacheKey'])) {
+                // write to cache
+                $key = $payload['cacheKey'];
+                $value = $payload['response'];
+                $this->_client->set($key, json_encode($value));
+                // set ttl
+                $ttl = isset($payload['configs']['cacheTTL']) ? $payload['configs']['cacheTTL'] : $this->_defaultCacheTTL;
+                $this->_client->expire($key, $ttl);
+            }
         }
-        // set cache for parent query
-        if (isset($payload["pipelineParams"]["parentRequestString"])) {
-          $parentCacheKey = $payload["pipelineParams"]["currentRequestString"].":parentQueries";
-          $this->_client->sAdd($parentCacheKey, $payload["pipelineParams"]["parentRequestString"]);
-        }
-        // set cache for object requests
-        $typeCacheKey = $type.":queries";
-        $this->_client->sAdd($typeCacheKey, $payload["pipelineParams"]["currentRequestString"]);
-      } else {
-        if (isset($payload["cacheKey"])) {
-          // write to cache
-          $key = $payload["cacheKey"];
-          $value = $payload["response"];
-          $this->_client->set($key,json_encode($value));
-        }
-      }
-      return $payload;
+
+        return $payload;
     }
 
-    private function execute($payload=[]) {
-      if (isset($payload["response"])) {
-        $cacheKey = $payload["type"].":queries";
-        $queries = $this->_client->smembers($cacheKey);
-        $keysToDelete = $this->getKeysToDelete($queries);
-        $resultKeys = $keysToDelete;
-        array_walk($resultKeys, function(&$value, $key) { $value .= ':response'; });
-        $parentKeys = $keysToDelete;
-        array_walk($parentKeys, function(&$value, $key) { $value .= ':parentQueries'; });
-        $numberOfDeletedKeys = $this->_client->del(array_values(array_merge($resultKeys,$parentKeys)));
-      }
-      return $payload;
-    }
-
-    private function getKeysToDelete($queries) {
-      $result = [];
-      foreach($queries as $query) {
-        $result[] = $query;
-        $cacheKey=$query.":parentQueries";
-        $parentList = $this->_client->smembers($cacheKey);
-        if (isset($parentList)) {
-          array_merge($result,$this->getKeysToDelete($parentList));
+    private function execute($payload = [])
+    {
+        if (isset($payload['response'])) {
+            $cacheKey = $payload['type'].':queries';
+            $queries = $this->_client->smembers($cacheKey);
+            $keysToDelete = $this->getKeysToDelete($queries);
+            $resultKeys = $keysToDelete;
+            array_walk($resultKeys, function (&$value, $key) { $value .= ':response'; });
+            $parentKeys = $keysToDelete;
+            array_walk($parentKeys, function (&$value, $key) { $value .= ':parentQueries'; });
+            $numberOfDeletedKeys = $this->_client->del(array_values(array_merge($resultKeys, $parentKeys)));
         }
-      }
-      return $result;
+
+        return $payload;
     }
 
+    private function getKeysToDelete($queries)
+    {
+        $result = [];
+        foreach ($queries as $query) {
+            $result[] = $query;
+            $cacheKey = $query.':parentQueries';
+            $parentList = $this->_client->smembers($cacheKey);
+            if (isset($parentList)) {
+                array_merge($result, $this->getKeysToDelete($parentList));
+            }
+        }
+
+        return $result;
+    }
 }
